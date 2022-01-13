1f5bae8bb1ee8d237770bfec3cf1f7b522eee4ad
Daniel Olson <emaildano@gmail.com>
2022-01-13 16:52:36 -0500

    updated parse function for fine tuning

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
+const fs = require("fs");
+const path = require('path');
 const mammoth = require("mammoth");
 const argv = require("minimist")(process.argv.slice(2));
 const file = argv?.file ? argv.file : "./datasets/file-sample.docx";
 
+let write = async ({ slug, content, flag }) => {
+  try {
+    if (!fs.existsSync(path.dirname(slug))) {
+      fs.mkdirSync(path.dirname(slug), { recursive: true });
+    }
+
+    fs.writeFileSync(slug, content, { flag: flag });
+    console.log(slug);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 mammoth.extractRawText({ path: `${file}` }).then(function (result) {
   console.log(result.value);
-  return result.value;
+
+  write({
+    slug: `./datasets/taps.prompt.txt`,
+    content: `${result.value}\n`,
+  });
+
+  write({
+    slug: `./datasets/taps.completion.txt`,
+    content: `${result.value}\n`,
+  });
 });
